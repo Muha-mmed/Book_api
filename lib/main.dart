efc214b78b9c6cbb2c3b11ed18844c1860811b6f
Muha-mmed <mrobiu001@gmail.com>
2024-12-16 02:51:04 -0800

    improving the code

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Harry Potter Books list"),
